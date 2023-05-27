a3105a808120003edbadd74fa4bc1313a9cf9e3a
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-27 20:31:08 +0100

    n

--- my-app/src/home/home.tsx
+++ my-app/src/home/home.tsx
@@ -3,24 +3,12 @@ import data from "../data/data";
 import logo from "../logo.svg";
 
 function Home() {
-  const [color, setColor] = useState("blue");
-
-  function handleClick() {
-    const changeCol = color === "blue" ? "red" : "black";
-    setColor(changeCol);
-  }
-
-  const paint = {
-    backgroundColor: color,
-  };
-
   return (
     <div className="bg-gray-600 h-screen w-full flex items-center justify-around">
-      <div className="w-72 h96 bg-white flex flex-col justify-between items-center">
+      <div className="w-72 h-96 bg-gray-700 flex text-white flex-col justify-between items-center">
         <img src={logo} alt="logo" />
-        <button onClick={handleClick} style={paint} className="w-24 h-16">
-          hey
-        </button>
+        <h1>country</h1>
+        <button className="py-3 px-6 bg-red-800">sign up</button>
       </div>
     </div>
   );
