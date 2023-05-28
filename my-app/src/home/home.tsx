28319aa5fe906f3ba520d53954e3fd4a5a09c87f
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-28 17:11:21 +0100

    ok doki

--- my-app/src/home/home.tsx
+++ my-app/src/home/home.tsx
@@ -4,17 +4,20 @@ import logo from "../logo.svg";
 
 function Home() {
   enum direction {
-    first,
-    second,
-    third,
+    first = "Pathetic",
+    second = "rosa",
+    third = "money",
   }
+
+  const sentence = direction.first + direction.third;
 
   return (
     <div className="bg-gray-600 h-screen w-full flex items-center justify-around">
       <div className="w-72 h-96 bg-gray-700 flex text-white flex-col justify-between items-center">
         <img src={logo} alt="logo" />
         <h1>country</h1>
-        <p></p>
+        <p>{direction.first}</p>
+        <span>{sentence}</span>
         <button className="py-3 px-6 bg-red-800">sign up</button>
       </div>
     </div>
