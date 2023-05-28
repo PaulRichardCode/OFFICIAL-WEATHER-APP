d9fa8cf2fe913fa439823dce7f30761bff3481d5
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-28 18:10:24 +0100

    paco

--- my-app/src/home/home.tsx
+++ my-app/src/home/home.tsx
@@ -6,19 +6,6 @@ import { log } from "console";
 function Home() {
   const [items, getItems] = useState("blue");
 
-  /* Write a function called sumArray which accepts a single argument: an array of numbers.  It should return the sum of all the numbers in the array.
-
-sumArray([1,2,3]) // 6
-sumArray([2,2,2,2]) // 8
-sumArray([50,50,1]) // 101
-Hints:
-
-You'll need a variable to keep track of the total.  It should start out as zero.
-
-Loop over the array and for each element, add it to the total variable.
-
-After you have added every number to total, return total. */
-
   function sumArray(nums: number[]) {
     let total: number = 0;
     for (let i = 0; i < nums.length; i++) {
