b02542e3203e9b5a3d3105bdb6d9bd855ef29e1d
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-28 17:00:06 +0100

    pa

--- my-app/src/home/home.tsx
+++ my-app/src/home/home.tsx
@@ -3,11 +3,18 @@ import data from "../data/data";
 import logo from "../logo.svg";
 
 function Home() {
+  enum direction {
+    first,
+    second,
+    third,
+  }
+
   return (
     <div className="bg-gray-600 h-screen w-full flex items-center justify-around">
       <div className="w-72 h-96 bg-gray-700 flex text-white flex-col justify-between items-center">
         <img src={logo} alt="logo" />
         <h1>country</h1>
+        <p></p>
         <button className="py-3 px-6 bg-red-800">sign up</button>
       </div>
     </div>
