82762eb1b489c5ccd86543c43389e50eb585df4d
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-30 13:20:24 +0100

    w

--- my-app/src/home/home.tsx
+++ my-app/src/home/home.tsx
@@ -1,19 +1,6 @@
-import React, { useState } from "react";
-import data from "../data/data";
-import logo from "../logo.svg";
-import { log } from "console";
+import React from "react";
 
 function Home() {
-  const [items, getItems] = useState("blue");
-
-  function sumArray(nums: number[]) {
-    let total: number = 0;
-    for (let i = 0; i < nums.length; i++) {
-      total += nums[i];
-    }
-    return total;
-  }
-
   return (
     <div className="bg-gray-600 h-screen w-full flex items-center justify-around">
       <div className="w-72 h-96 bg-gray-700 flex text-white flex-col justify-between items-center">
