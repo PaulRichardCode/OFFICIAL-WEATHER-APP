66b704e21ae1446dbe692a8b4116df6c98b556cf
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-30 13:55:38 +0100

    s

--- my-app/src/home/home.tsx
+++ my-app/src/home/home.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import logo from "../data/glook.jpg";
 
 function Home() {
   return (
-    <div className="bg-gray-600 h-screen w-full flex items-center justify-around">
-      <div className="w-72 h-96 bg-gray-700 flex text-white flex-col justify-between items-center">
-        <img src={logo} alt="logo" />
-        <h1>country</h1>
-        <button className="py-3 px-6">sign up</button>
-        <p>{sumArray([3, 4, 5])}</p>
+    <div className="bg-gradient-to-b to-cyan-400 from-blue-600 h-screen w-full flex flex-col items-center justify-center">
+      <div>
+        <h1 className="text-2xl text-center"> Meme Generator</h1>
+        <div className="max-w-xs">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="flex item-center justify-between py-1">
+          <button className="py-4 px-6 rounded-md bg-slate-400">
+            Download meme
+          </button>
+          {/* install react icons */}
+          <button className="py-4 px-6 rounded-md bg-slate-400">Share</button>
+        </div>
       </div>
+
+      <footer>Powered by Paul Richard</footer>
     </div>
   );
 }
