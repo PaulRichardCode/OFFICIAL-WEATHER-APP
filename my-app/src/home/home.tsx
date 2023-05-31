c3c2c413c1bfe737701775d579a5908bf5d03342
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-31 14:11:14 +0100

    aa

--- my-app/src/home/home.tsx
+++ my-app/src/home/home.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import logo from "../data/glook.jpg";
-import { BsEmojiLaughing } from "react-icons/bs";
+import { FcSearch } from "react-icons/fc";
 ///BsSearch FcSearch HiOutlineArrowRightOnRectangle
 // FcGallery GrGallery
 //MdOutlineTextRotationNone
@@ -12,7 +11,19 @@ function Home() {
   return (
     <div className="bg-gradient-to-b from-cyan-50 to-indigo-100 h-screen w-full flex flex-col items-center justify-center">
       <div>
-        <h1 className="text-2xl text-center"> Meme Generator</h1>
+        {" "}
+        <label
+          htmlFor="search"
+          className="flex items-center px-5 py-1 bg-yellow-400 ">
+          {" "}
+          <input
+            type="search"
+            id="search"
+            placeholder="Search Memes"
+            className="bg-yellow-400 px-5 placeholder:text-blue-600 "
+          />{" "}
+          <FcSearch />
+        </label>
       </div>
     </div>
   );
