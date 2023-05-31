f26a54542b542b5cfb96891d3e48505ff9f64b19
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-05-31 12:48:36 +0100

    s

--- my-app/src/nav/nav.jsx
+++ my-app/src/nav/nav.jsx
@@ -3,6 +3,7 @@ import Logo from "../data/meme-logo.png";
 import { useState } from "react";
 import { CgMenuRightAlt } from "react-icons/cg";
 import { BsArrowRightShort } from "react-icons";
+import { FcGallery } from "react-icons/fc";
 
 const Nav = () => {
   /* Functionality */
@@ -16,29 +17,32 @@ const Nav = () => {
   /* Tailwind jsx */
   return (
     <div className="">
-      <nav class="px-2 sm:py-0 py-2 sm:px-0 fixed w-full z-20 top-0 left-0 shadow-sm shadow-black border-gray-200 dark:border-gray-600">
-        <div class="container flex flex-wrap items-center justify-between mx-auto">
+      <nav class="px-2 sm:py-0 py-2 sm:px-2 fixed w-full top-0 left-0 border-gray-200 dark:border-gray-600">
+        <div class="flex flex-wrap items-center justify-between mx-auto">
           {/* logo */}
           <a href="#" class="flex items-center">
             <img
               src={Logo}
               width={70}
-              className="md:w-16 w-12"
+              className="md:w-24 w-12 "
               alt="Flowbite Logo"
             />
+            <p className="text-2xl text-blue-600 flex items-center">
+              RICH <FcGallery /> MEME
+            </p>
           </a>
           <div class="flex md:order-2">
             {/* responsive button */}
             <button
-              onMouseDown={toggleMenu}
+              onMouseOver={toggleMenu}
               id="btn"
               data-dropdown-toggle="navbar-sticky"
               type="button"
-              class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+              class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-sticky"
               aria-expanded="true">
               <span class="sr-only">Open main menu</span>
-              <CgMenuRightAlt className="text-4xl" />
+              <CgMenuRightAlt className="text-4xl text-blue-600" />
             </button>
           </div>
 
