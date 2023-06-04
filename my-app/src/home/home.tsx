4e5319381657affc426c3242ac5ed4d54d014692
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-06-04 18:31:34 +0100

    integrating api

--- my-app/src/home/home.tsx
+++ my-app/src/home/home.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { log } from "console";
+import React, { useEffect, useState } from "react";
 import { CgClose, CgSearch, CgSmile } from "react-icons/cg";
 import { FcSearch } from "react-icons/fc";
 
@@ -11,10 +12,25 @@ import { FcSearch } from "react-icons/fc";
 
 function Home() {
   const [openMeme, setOpenMeme] = useState(false);
+  const [dataMeme, setDataMeme] = useState(null);
 
   const displayMeme = () => {
     setOpenMeme(!openMeme);
   };
+
+  useEffect(() => {
+    const fetchMemeData = async () => {
+      try {
+        const response = await fetch("https://api.imgflip.com/get_memes");
+        const data = await response.json();
+        setDataMeme(data);
+      } catch (error) {
+        console.log("error fetching meme data");
+      }
+    };
+
+    fetchMemeData();
+  }, []);
 
   return (
     <div className=" h-screen w-full flex flex-col items-center justify-center">
@@ -72,11 +88,13 @@ function Home() {
             {/* API INTEGRATIONS */}
             <div className="flex w-full justify-start items-start">
               <div className="flex flex-col items-center w-48 h-auto my-5 bg-white-100">
-                <h1 className="text-center text-sm">drake hotline bling</h1>
+                {dataMeme ? <div></div> : <div></div>}
+
+                {/* <h1 className="text-center text-sm">drake hotline bling</h1>
 
                 <button className="bg-blue-600 w-32 py-1 text-sm text-white rounded-md border-blue-600 border">
                   Use this image
-                </button>
+                </button> */}
               </div>
             </div>
           </div>
