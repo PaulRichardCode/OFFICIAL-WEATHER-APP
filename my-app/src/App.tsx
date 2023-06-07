213388cefa7e3af0d7249af26fa20518d3838d49
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-06-07 13:09:27 +0100

    b

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,42 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
-
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-} from "./components";
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-        <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
-    </BrowserRouter>
-  );
-};
-
-export default App;
-
-/* import React from "react";
+import React from "react";
 import Home from "./home/home";
 import Nav from "./nav/nav";
 
@@ -50,4 +12,3 @@ function App() {
 }
 
 export default App;
- */
