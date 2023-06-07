ece6bbabb0e21b4deee75c4bf5d6d0b65c3de780
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2023-06-07 13:06:49 +0100

    SQS

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,42 @@
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
+/* import React from "react";
 import Home from "./home/home";
 import Nav from "./nav/nav";
 
@@ -12,3 +50,4 @@ function App() {
 }
 
 export default App;
+ */
